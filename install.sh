123c5d7b165e32fe7a25edc1dc75951c67966887
Systemkeyy <74800251+systemkeyy@users.noreply.github.com>
2022-02-28 02:32:24 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,42 @@
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m'PHP AUTOMATION - SINCE ITS IMPOSSIBLE NOT TO HATE TO FORGET AN EXT'\033[0m' 
+echo"S"
+sleep 0.1
+echo"Y"
+sleep 0.1
+echo"S"
+sleep 0.1
+echo"T"
+sleep 0.1
+echo"E"
+sleep 0.1
+echo"M"
+sleep 0.1
+echo"K"
+sleep 0.1
+echo"e"
+sleep 0.1
+echo"y"
+sleep 0.1
+echo"y"
+sleep 5
+sudo apt -y update && sudo apt -y
+sudo apt -y install software-properties-common &&
+sleep 1 &&
+sudo add-apt-repository ppa:ondrej/php &&
+sudo apt-get update &&
+sleep 1
+clear &&
+echo -ne $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m'Checking current status, and continuing.'\033[0m' 
+sleep 5
+sudo apt -y install php7.4 &&
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m'Your current PHP Version is being checked'\033[0m' 
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m'Installing'\033[0m' 
+php -v &&
+sleep 3 
+read -p "Buy me a beer? Yes/No " prompt
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+then
+sudo apt-get install -y php7.4-cli php7.4-json php7.4-common php7.4-mysql php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath php7.4-cli php7.4-json php7.4-common php7.4-mysql php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath;
+ else
+    echo "Fuck off then"
+ fi
